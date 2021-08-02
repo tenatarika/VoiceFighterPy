41bc83cbe05d1b844eb5574c8187e01378144743
Furcas <tenatarika@gmail.com>
2021-08-02 04:11:57 +0300

    refactor/ add file for testing voicer\

---
+++ test_voice.py
@@ -0,0 +1,31 @@
+from voice_Iissener import VoiceLissener
+from voice_speaker import  Speaker
+
+
+
+def main():
+    
+    lissener = VoiceLissener()
+    speaker = Speaker()
+    
+    speaker.speak("Вы хотите разрушить мир?")
+    take = lissener.takeCommands()
+    
+    choice = take
+    if "yes" in choice:
+        speaker.speak("Ваш мир будет разрушен через 10 секунд")
+        for i in range(9, 0, -1):
+            
+            speaker.speak(i)
+        
+        
+    if "no" in choice:
+        speaker.speak("thanks")
+        
+        
+    
+    
+
+if __name__ == '__main__':
+    main()
+    
